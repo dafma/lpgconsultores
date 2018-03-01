a7a49c79ba92a09c376d6a723aef2b3004b7b99e
dafma <mar_tp@hotmail.es>
2018-02-28 21:41:41 -0600

    pueba

---
+++ js/servicioselec.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+	$("#sebtm0").click(function(){
+		$("#servicio0").show("slow");
+	});
+	$("#sebto0").click(function(){
+		$("#servicio0").hide("slow");
+	});
+
+
+
+	$("#sebtm1").click(function(){
+		$("#servicio1").show("slow");
+	});
+	$("#sebto1").click(function(){
+		$("#servicio1").hide("slow");
+	});
+
+
+
+	$("#sebtm2").click(function(){
+		$("#servicio2").show("slow");
+	});
+	$("#sebto2").click(function(){
+		$("#servicio2").hide("slow");
+	});
+});
---
+++ js/verticales.js
@@ -0,0 +1,81 @@
+
+$(document).ready (function(){
+	$("#btm0").click(function(){
+		$("#text0").show("slow");
+	});
+	$("#bto0").click(function(){
+		$("#text0").hide("slow");
+	});
+
+
+
+	$("#btm1").click(function(){
+		$("#text1").show("slow");
+	});
+	$("#bto1").click(function(){
+		$("#text1").hide("slow");
+	});
+
+
+
+	$("#btm2").click(function(){
+		$("#text2").show("slow");
+	});
+	$("#bto2").click(function(){
+		$("#text2").hide("slow");
+	});
+
+
+
+	$("#btm3").click(function(){
+		$("#text3").show("slow");
+	});
+	$("#bto3").click(function(){
+		$("#text3").hide("slow");
+	});
+
+
+
+	$("#btm4").click(function(){
+		$("#text4").show("slow");
+	});
+	$("#bto4").click(function(){
+		$("#text4").hide("slow");
+	});
+
+
+
+	$("#btm5").click(function(){
+		$("#text5").show("slow");
+	});
+	$("#bto5").click(function(){
+		$("#text5").hide("slow");
+	});
+
+
+
+	$("#btm6").click(function(){
+		$("#text6").show("slow");
+	});
+	$("#bto6").click(function(){
+		$("#text6").hide("slow");
+	});
+
+
+
+	$("#btm7").click(function(){
+		$("#text7").show("slow");
+	});
+	$("#bto7").click(function(){
+		$("#text7").hide("slow");
+	});
+
+
+
+	$("#btm8").click(function(){
+		$("#text8").show("slow");
+	});
+	$("#bto8").click(function(){
+		$("#text8").hide("slow");
+	});
+});
